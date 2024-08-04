fdf60912a6fb268a13d887eea7859a282b0277b8
cibele barbosa <cbsilva@magnasistemas.com.br>
2024-08-03 23:33:40 -0300

    feat: Desenvolvimento

---
+++ server/Models/Clientes.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace server.Models
+{
+    public class Clientes
+    {
+
+        [Key]
+        [Required]
+        public int client_id { get; set; }
+        [ForeignKey("users")]
+        public int user_id { get; set; }
+
+        [Required(ErrorMessage = "O campo nome é obrigatório")]
+        public string nome { get; set; }
+        [Required(ErrorMessage = "O campo documento é obrigatório")]
+        public string documento { get; set; }
+        [Required(ErrorMessage = "O campo telefone é obrigatório")]
+        public string telefone { get; set; }
+        [Required(ErrorMessage = "O campo endereco é obrigatório")]
+        public string endereco { get; set; }
+    }
+}
---
+++ server/Models/Cobrancas.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace server.Models
+{
+    public class Cobrancas
+    {
+        [Key]
+        [Required]
+        public int cobranca_id { get; set; }
+        [ForeignKey("users")]
+        public int user_id { get; set; }
+        [ForeignKey("clientes")]
+        public int client_id { get; set; }
+
+        [Required(ErrorMessage = "O campo descricao é obrigatório")]
+        public string descricao { get; set; }
+        [Required(ErrorMessage = "O campo valor é obrigatório")]
+        public decimal valor { get; set; }
+        [Required(ErrorMessage = "O campo data é obrigatório")]
+        public DateTime data { get; set; }
+        [Required(ErrorMessage = "O campo pago é obrigatório")]
+        public Boolean pago { get; set; }
+    }
+
+}
